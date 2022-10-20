cb38f22c0b94a7bb75e4fdd2a1f740fe257f9798
Darlene Leão <darlene_leao@outlook.com>
2022-10-20 09:25:43 -0300

    criando projeto na prereragem

--- src/main/java/academy/devdojo/springboot2/start/AplicationStart.java
+++ src/main/java/academy/devdojo/springboot2/start/AplicationStart.java
@@ -1,5 +1,17 @@
 package academy.devdojo.springboot2.start;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "academy.devdojo.springboot2")
 public class AplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(AplicationStart.class,args);
+
+    }
+
 
 }
