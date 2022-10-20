5c7d32b27b99087d9578bf4221a5567e1cf463f4
Darlene Leão <darlene_leao@outlook.com>
2022-10-20 14:31:26 -0300

    subindo alterações

--- src/main/java/academy/devdojo/springboot2/AplicationStart.java
+++ src/main/java/academy/devdojo/springboot2/AplicationStart.java
@@ -1,12 +1,12 @@
-package academy.devdojo.springboot2.start;
+package academy.devdojo.springboot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "academy.devdojo.springboot2")
+@SpringBootApplication
 public class AplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(AplicationStart.class,args);
--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,18 +1,33 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.DateUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
+@AllArgsConstructor
 public class AnimeController{
+    private DateUtil dateUtil;
     //localhost:8080/anime/list
     @GetMapping(path = "list")
     public List<Anime> list(){
+        log.info(dateUtil.formatLocalDataTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Anime("DBZ"), new Anime( "Berserk"));
+    }
+
+    @GetMapping(path = "list2")
+    public List<Anime> list2(){
+        log.info(dateUtil.formatLocalDataTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("DBZ"), new Anime( "Berserk"), new Anime( "One Piece"));
     }
 }
